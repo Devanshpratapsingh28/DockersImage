a5a19f1ddf69cd382dcffe793f374a4022976c9c
Devansh <devanshprataps6@gmail.com>
2025-10-21 13:14:35 +0530

    added comment in test_app.py file

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import app
 
 def test_home():
     response = app.test_client().get("/")
-
-    assert response.status_code == 200
-    assert response.get_data(as_text=True) == "<h1>Hello World!</h1>"
+    # assert is used to verify the expected outcome
+    assert response.status_code == 200 # Check if the response status code is 200 (OK)
+    assert response.get_data(as_text=True) == "<h1>Hello World!</h1>" # Check if the response data matches the expected output
